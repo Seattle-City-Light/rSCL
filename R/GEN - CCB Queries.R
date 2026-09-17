9a518c228375ecc48b9a4f5239d5a82a9b6ff901
Dangatang <38509928+matthewhamlin@users.noreply.github.com>
2026-09-17 14:17:01 -0700

    Add files via upload

--- R/GEN - CCB Queries.R
+++ R/GEN - CCB Queries.R
@@ -107,3 +107,113 @@ ccb_check_valid_id_type <- function(id_type = 'PREM_ID'){
 
 
 
+
+
+
+
+#' CCB Query that maps rate codes to their full description
+#'
+#' @param connect True if you want the function to make the connection to CCB
+#' @return Data frame with rate codes mapped to their full description
+#' @export
+scl_rate_code_descr <- function(connect = T){
+
+
+  if(connect){
+    scl_connect('CCB')
+  }
+
+  rs_l <- DBI::dbGetQuery(con,"Select RS_CD, DESCR
+                          From CISADM.CI_RS_L
+                          Where RS_CD like 'E%'")
+
+  rs_l$DESCR <- toupper(rs_l$DESCR)
+  rs_l$DESCR2 <- rs_l$DESCR
+
+  data.table::setDT(rs_l)
+  rs_l[,DESCR := gsub('SCL ','', DESCR)]
+  rs_l[,DESCR := gsub('ELECTRIC ','', DESCR)]
+  rs_l[grepl('RESIDENTIAL', DESCR) ,CLASS := 'RESIDENTIAL'][is.na(CLASS),CLASS := 'COMMERCIAL'][,DESCR := gsub('RESIDENTIAL: ','', DESCR)]
+  rs_l[CLASS == 'RESIDENTIAL', RATE_CLASS := 'RESIDENTIAL'][grepl('SMALL', DESCR), RATE_CLASS := 'SMALL'][grepl('MEDIUM', DESCR), RATE_CLASS := 'MEDIUM'][grepl('LARGE', DESCR), RATE_CLASS := 'LARGE'][grepl('HIGH DEMAND', DESCR), RATE_CLASS := 'HIGH DEMAND']
+
+  rs_l[,DESCR := gsub('GENERAL SERVICE: ','', DESCR)]
+  rs_l[,DESCR := gsub('MEDIUM ','', DESCR)]
+  rs_l[,DESCR := gsub('SMALL ','', DESCR)]
+  rs_l[,DESCR := gsub('LARGE ','', DESCR)]
+  rs_l[,DESCR := gsub('HIGH DEMAND ','', DESCR)]
+
+  rs_l[grepl('STREETLIGHTS', DESCR), RATE_CLASS := 'STREETLIGHTS']
+  rs_l[grepl('FLAT', DESCR), RATE_CLASS := 'FLAT']
+  rs_l[grepl('PRODUCTION', DESCR), RATE_CLASS := 'PRODUCTION']
+  rs_l[grepl('SELF CONSUMING', DESCR), RATE_CLASS := 'SELF CONSUMING']
+
+  rs_l[,DESCR := gsub('STREETLIGHTS ','', DESCR)][,DESCR := gsub('FLAT ','', DESCR)][,DESCR := gsub('PRODUCTION ','', DESCR)][,DESCR := gsub('SELF CONSUMING ','', DESCR)]
+
+  rs_l[,DESCR := gsub('STANDARD GENERAL SERVICE ','', DESCR)][,DESCR := gsub('STANDARD ','', DESCR)]
+
+  rs_l[grepl('RECEIVED', DESCR), SALE_TYPE := 'RECEIVED'][is.na(SALE_TYPE), SALE_TYPE := 'DELIVERED']
+
+  rs_l[,DESCR := gsub('(RECEIVED): ','', DESCR)][,DESCR := gsub('(RECEIVED):','', DESCR)]
+
+  rs_l[grepl('NON-RESIDENTIAL', DESCR), CLASS := 'COMMERCIAL'][grepl('GREEN UP', DESCR), RATE_CLASS := 'GREEN UP']
+
+  rs_l[,DESCR := gsub('NON-RESIDENTIAL ','', DESCR)][,DESCR := gsub('RESIDENTIAL ','', DESCR)][,DESCR := gsub('- GREEN UP - ','', DESCR)]
+
+  rs_l[RATE_CLASS =='PRODUCTION', SALE_TYPE := 'DELIVERED/RECEIVED']
+
+  rs_l[grepl('BIMONTHLY|BI-MONTHLY', DESCR), BILL_FREQ := 'BIMONTHLY'][is.na(BILL_FREQ), BILL_FREQ:='MONTHLY']
+
+  rs_l[,DESCR := gsub('BIMONTHLY','', DESCR)][,DESCR := gsub('BI-MONTHLY','', DESCR)][,DESCR := gsub('MONTHLY','', DESCR)]
+
+  rs_l[grepl('PILOT', DESCR), PROGRAM := 'PILOT'][grepl('COMMERCIAL CHARGING', DESCR), PROGRAM := 'COMMERCIAL CHARGING'][is.na(PROGRAM), PROGRAM:='STANDARD']
+
+  rs_l[,DESCR := gsub('INTERVAL','', DESCR)][,DESCR := gsub('INTEVAL','', DESCR)][,DESCR := gsub('PILOT','', DESCR)][,DESCR := gsub('COMMERCIAL CHARGING','', DESCR)]
+
+  rs_l[grepl('TOU', DESCR), RATE_TYPE := 'TOU'][is.na(RATE_TYPE), RATE_TYPE:='STANDARD']
+
+  rs_l[,DESCR := gsub('TOU','', DESCR)]
+
+  rs_l[grepl('BURIEN', DESCR), SERVICE_AREA := 'BURIEN']
+  rs_l[grepl('CITY', DESCR), SERVICE_AREA := 'CITY']
+  rs_l[grepl('KING COUNTY', DESCR), SERVICE_AREA := 'KING COUNTY']
+  rs_l[grepl('LAKE FOREST PARK|LFP', DESCR), SERVICE_AREA := 'LAKE FOREST PARK']
+  rs_l[grepl('NETWORK', DESCR), SERVICE_AREA := 'NETWORK']
+  rs_l[grepl('NORMANDY PARK', DESCR), SERVICE_AREA := 'NORMANDY PARK']
+  rs_l[grepl('SEATAC', DESCR), SERVICE_AREA := 'SEATAC']
+  rs_l[grepl('SHORELINE', DESCR), SERVICE_AREA := 'SHORELINE']
+  rs_l[grepl('SUBURBAN|SUBRBAN', DESCR), SERVICE_AREA := 'SUBURBAN']
+  rs_l[grepl('TUKWILA', DESCR), SERVICE_AREA := 'TUKWILA']
+
+  rs_l[,DESCR := gsub('BURIEN','', DESCR)]
+  rs_l[,DESCR := gsub('CITY','', DESCR)]
+  rs_l[,DESCR := gsub('KING COUNTY','', DESCR)]
+  rs_l[,DESCR := gsub('LAKE FOREST PARK','', DESCR)]
+  rs_l[,DESCR := gsub('LFP','', DESCR)]
+  rs_l[,DESCR := gsub('NETWORK','', DESCR)]
+  rs_l[,DESCR := gsub('NORMANDY PARK','', DESCR)]
+  rs_l[,DESCR := gsub('SEATAC','', DESCR)]
+  rs_l[,DESCR := gsub('SHORELINE','', DESCR)]
+  rs_l[,DESCR := gsub('SUBURBAN','', DESCR)]
+  rs_l[,DESCR := gsub('SUBRBAN','', DESCR)]
+  rs_l[,DESCR := gsub('TUKWILA','', DESCR)]
+
+  rs_l[RS_CD =='EFLTRB  ', BILL_FREQ := 'BIMONTHLY']
+  rs_l[RS_CD =='ERLIB   ', BILL_FREQ := 'BIMONTHLY']
+
+  rs_l[RATE_CLASS == 'GREEN UP', PROGRAM := 'GREEN UP'][RATE_CLASS == 'GREEN UP', SERVICE_AREA := 'GREEN UP']
+  rs_l[RATE_CLASS == 'STREETLIGHTS', SERVICE_AREA := 'STREETLIGHTS'][RATE_CLASS == 'FLAT', SERVICE_AREA := 'FLAT'][RATE_CLASS == 'PRODUCTION', SERVICE_AREA := 'PRODUCTION'][RATE_CLASS == 'SELF CONSUMING', SERVICE_AREA := 'SELF CONSUMING']
+
+  rs_l$DESCR <- rs_l$DESCR2
+  rs_l$DESCR2 <- NULL
+
+  `%>%` <- dplyr::`%>%`
+
+  rs_l <- rs_l %>%
+    dplyr::select(RS_CD, DESCR, CLASS, RATE_CLASS, RATE_TYPE, SERVICE_AREA, BILL_FREQ, PROGRAM, SALE_TYPE) %>%
+    dplyr::rename(RS_DESCR = DESCR)
+
+  trimws_custom(rs_l)
+
+  return(rs_l)
+
+}
